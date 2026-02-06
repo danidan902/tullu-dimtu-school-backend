cc395b2d04fd45c15fdb0a5ebf303b6ea75442ee
Daniel Sheleme <danid93575@gmail.com>
2026-02-06 13:23:36 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -51,7 +51,7 @@ cloudinary.config({
 // Create Socket.io server for real-time
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://tuludimtuschool.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -62,7 +62,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://tuludimtuschool.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS",'PATCH',],
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true
