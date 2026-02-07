d824c16d83d7350d27a01c6c4e45195d95a1a1a3
Daniel Sheleme <danid93575@gmail.com>
2026-02-07 11:41:21 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -33,7 +33,7 @@ const verifyServiceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
 const twilioClient = twilio(accountSid, authToken);
 
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 'https://tuludimtuschool.vercel.app/' ; 
 const app = express();  
 const server = createServer(app);
 
@@ -51,7 +51,7 @@ cloudinary.config({
 // Create Socket.io server for real-time
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://tuludimtuschool.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -62,7 +62,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://tuludimtuschool.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS",'PATCH',],
   allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true
